6eb5c5cdcc3c69779dbe0bd4dac5434a08365e89
H-Ojun <52265076+H-Ojun@users.noreply.github.com>
2023-02-13 14:13:42 +0900

    commit

---
+++ delivery/src/main/java/mall/domain/DeliveryComplete.java
@@ -0,0 +1,26 @@
+package mall.domain;
+
+import mall.domain.*;
+import mall.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+
+@Data
+@ToString
+public class DeliveryComplete extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Integer qty;
+    private String productName;
+    private String status;
+
+    public DeliveryComplete(Delivery aggregate){
+        super(aggregate);
+    }
+    public DeliveryComplete(){
+        super();
+    }
+}
